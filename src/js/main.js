447f6f68b44ec27195cd58d7e3a0ccb89e666764
fregire <fregire@mail.ru>
2018-07-01 20:40:50 +0500

    Начальное положение

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,6 @@
-$(document).ready(function() {
-	// Активация слайдеров
-
-	// Актвивация слайдера работ
-	$(".examples__slider").slick({
-		dots: true,
-		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
-    	nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>',
-    	responsive: [
-	    	{
-	    		breakpoint: 1000,
-	    		settings: {
-	    			slidesToShow: 2,
-	    			centerMode: true,
-	    			vertical: false,
-	    			dots: true
-	    		}
-	    	},
-	    	{
-	    		breakpoint: 760,
-	    		settings: {
-	    			slidesToShow: 1,
-	    			centerMode: true,
-	    			vertical: false,
-	    			dots: true
-	    		}
-	    	},
-	    	{
-	    		breakpoint: 480,
-	    		settings: {
-	    			slidesToShow: 1,
-	    			centerMode: false,
-	    			vertical: false,
-	    		}
-	    	}
-
-    	],
-    	slidesToShow: 3,
-    	vertical: true,
-    	centerMode: false,
-    	dots: false
-	});
-
-	// Активация слайдера трансляций
-	$(".gallery-slider__slides").slick({
-		dots: true,
-		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
-    nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
-	});
-	// Галлерея
-	$('.video__link').magnificPopup({
-	  type: 'image',
-	  gallery: {
-	      enabled:true
-	  }
+$(document).ready(function(){
+	$(".slider-head").slick({
+		slidesToShow: 1,
+		fade: true
 	});
 });
