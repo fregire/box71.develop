c044d788864b88baa7ef50ca83e1b2843cb5c68b
fregire <fregire@mail.ru>
2018-07-02 15:37:47 +0500

    Верстка Из чего мы строим

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$(".tabs").slick({
-		slidesToShow: 4
+		slidesToShow: 4,
+		prevArrow: "<button class='tabs__btn btn btn--arrow btn--prev'>Назад</button>",
+		nextArrow: "<button class='tabs__btn btn btn--arrow btn--next'>Еще</button>",
 	})
 });
