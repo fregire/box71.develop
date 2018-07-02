d42c50e14f3c610d325f17721430dbde5647d91d
fregire <fregire@mail.ru>
2018-07-02 22:56:38 +0500

    Верстка Наших проектов

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,11 @@ $(document).ready(function(){
 		slidesToShow: 4,
 		prevArrow: "<button class='tabs__btn btn btn--arrow btn--prev'>Назад</button>",
 		nextArrow: "<button class='tabs__btn btn btn--arrow btn--next'>Еще</button>",
+	});
+
+	$(".projects__list").slick({
+		slidesToShow: 4,
+		prevArrow: "<button class='projects__arrow btn btn--arrow btn--prev'>Назад</button>",
+		nextArrow: "<button class='projects__arrow btn btn--arrow btn--next'>Еще</button>",
 	})
 });
