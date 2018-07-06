e09c7ef9f9adef397f60e308b2714387d7cd25d8
fregire <fregire@mail.ru>
2018-07-06 22:41:46 +0500

    Почти адаптировал,  еще для самих мобильных

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,9 @@
 $(".menu__phone-trigger").click(function(){
 	$(".nav").addClass("nav--opened");
 });
+
+$(document).click(function(event) {
+    if ($(event.target).closest(".menu").length) return;
+    $(".nav").removeClass("nav--opened");
+    event.stopPropagation();
+});
